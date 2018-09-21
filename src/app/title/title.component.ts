8c7f548949c3eb30d450848f65371abb83c3afdd
rene <rhernandez@novasolutionsystemscom>
2018-09-21 17:52:10 -0500

    update

--- src/app/title/title.component.ts
+++ src/app/title/title.component.ts
@@ -5,16 +5,48 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './title.component.html',
   styleUrls: ['./title.component.css']
 })
+export class TitleComponent implements OnInit{
+  public name : string;
+  public moreInformation : boolean = false;
+  public isAvailable : boolean = true;
+  public topics : string[];
+  public category : string;  // mobile, web, other
+
+  constructor(){}
+
+  ngOnInit(){
+    this.category = 'web';
+    this.name = 'Reni';
+    this.topics = [
+      'Fundamentos del framework',
+      'Componentes',
+      'Directivas',
+      'Animaciones',
+      'Servicios',
+      'RxJS',
+      'Binding',
+    ];
+  }
+}
+
+
+
+/*
+ngIf
 export class TitleComponent implements OnInit {
   public name : string;
   public edad : number;
   public edades : number[];
+
+  public moreInformation : boolean = false;
 
   constructor() { }
 
   ngOnInit() {
     this.name = 'Reni';
     this.edad = 37;
+
+    setTimeout(()=> this.moreInformation = true, 3000);
   }
 
   getEdadNombre() : string {
@@ -22,3 +54,4 @@ export class TitleComponent implements OnInit {
   }
 
 }
+*/
